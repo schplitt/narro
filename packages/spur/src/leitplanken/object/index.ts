8bff30385025f6ded592fad473ade7a9b25c351a
schplitt <schplitt.contact@gmail.com>
2025-09-06 22:07:19 +0200

    object rewrite

--- packages/spur/src/leitplanken/object/index.ts
+++ packages/spur/src/leitplanken/object/index.ts
@@ -1,28 +1,27 @@
-import type { BuildableSchema, Checkable } from '../../types/schema'
-import { number } from '../number'
-import { string } from '../string'
+import type { CommonOptions, DefaultCommonOptions, ExtractNullableSchema, ExtractNullishSchema, ExtractOptionalSchema, ExtractRequiredSchema, MakeNullable, MakeNullish, MakeOptional, MakeRequired } from '../../types/common'
+import type { BuildableSchema } from '../../types/schema'
+import type { ExtractOutputType } from '../../types/utils'
 
-export interface ObjectSchema<TOutput extends object> extends BuildableSchema<TOutput> {
+export type ObjectEntries = Record<string, BuildableSchema<unknown, unknown, CommonOptions>>
 
+type InferObjectOutput<T extends ObjectEntries> = {
+  [K in keyof T as T[K] extends ExtractRequiredSchema<T[K]> ? K : never]: NonNullable<ExtractOutputType<T[K]>>
+} & {
+  [K in keyof T as T[K] extends ExtractOptionalSchema<T[K]> ? K : never]?: NonNullable<ExtractOutputType<T[K]>>
+} & {
+  [K in keyof T as T[K] extends ExtractNullableSchema<T[K]> ? K : never]: ExtractOutputType<T[K]> | null
+} & {
+  [K in keyof T as T[K] extends ExtractNullishSchema<T[K]> ? K : never]: ExtractOutputType<T[K]> | undefined | null
 }
 
-type InferObjectOutput<T extends ObjectStructure> = {
-  [K in keyof T]: T[K] extends BuildableSchema<infer U> ? U : never
+export interface ObjectSchema<TOutput = object, TInput = object, TCommonOptions extends CommonOptions = DefaultCommonOptions> extends BuildableSchema<TOutput, TInput, TCommonOptions> {
+
+  optional: () => ObjectSchema<TOutput | undefined, TInput, MakeOptional<TCommonOptions>>
+  required: () => ObjectSchema<NonNullable<TOutput>, TInput, MakeRequired<TCommonOptions>>
+  nullable: () => ObjectSchema<TOutput | null, TInput, MakeNullable<TCommonOptions>>
+  nullish: () => ObjectSchema<TOutput | null | undefined, TInput, MakeNullish<TCommonOptions>>
 }
 
-export type ObjectStructure = Record<string, BuildableSchema>
-
-export function object<const TStructure extends ObjectStructure>(structure: TStructure): ObjectSchema<InferObjectOutput<TStructure>> {
-  const initialCheck: Checkable<object> = {
-    '~i': () => import('./object'),
-    '~id': () => import('./object').then(m => m.objectSymbol),
-    '~c': v => import('./object').then(m => m.checkObject(v)),
-  }
-
-  return {} as any
+export function object<T extends ObjectEntries>(structure: T): ObjectSchema<InferObjectOutput<T>> {
+  return 1 as any
 }
-
-const s = object({
-  name: string().minLength(3).maxLength(30),
-  age: number().min(0).max(150),
-})
