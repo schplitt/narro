45e66d1ef92f42c93f0ffe37241a3519cb3b4909
schplitt <schplitt.contact@gmail.com>
2025-09-06 22:07:08 +0200

    number rewrite

--- packages/spur/src/leitplanken/number/index.ts
+++ packages/spur/src/leitplanken/number/index.ts
@@ -1,40 +1,19 @@
-import type { BuildableSchema, Checkable } from '../../types/schema'
+import type { CommonOptions, DefaultCommonOptions, MakeNullable, MakeNullish, MakeOptional, MakeRequired } from '../../types/common'
+import type { BuildableSchema } from '../../types/schema'
 
-export interface NumberSchema<TOutput extends number = number> extends BuildableSchema<TOutput> {
-  min: (min: number) => NumberSchema<TOutput>
-  max: (max: number) => NumberSchema<TOutput>
+export interface NumberSchema<TOutput = number, TInput = number, TCommonOptions extends CommonOptions = DefaultCommonOptions> extends BuildableSchema<TOutput, TInput, TCommonOptions> {
+  min: (min: number) => NumberSchema<TOutput, TInput, TCommonOptions>
+  max: (max: number) => NumberSchema<TOutput, TInput, TCommonOptions>
+
+  default: <TDefault extends number>(v: TDefault) => NumberSchema<number, number | undefined, TCommonOptions>
+  unsetDefault: () => NumberSchema<number, number, TCommonOptions>
+
+  optional: () => NumberSchema<number | undefined, number | undefined, MakeOptional<TCommonOptions>>
+  required: () => NumberSchema<number, number, MakeRequired<TCommonOptions>>
+  nullable: () => NumberSchema<number | null, number | null, MakeNullable<TCommonOptions>>
+  nullish: () => NumberSchema<number | undefined | null, number | undefined | null, MakeNullish<TCommonOptions>>
 }
 
-
 export function number(): NumberSchema {
-  const initialCheck: Checkable<number> = {
-    '~i': () => import('./number'),
-    '~id': () => import('./number').then(m => m.numberSymbol),
-    '~c': v => import('./number').then(m => m.checkNumber(v)),
-  }
-
-  const cs: Checkable<number>[] = []
-  const schema: NumberSchema = {
-    'min': (min: number) => {
-      cs.push({
-        '~i': () => import('./min'),
-        '~id': () => import('./min').then(m => m.minSymbol),
-        '~c': (input: number) => import('./min').then(m => m.buildMinCheck(min)(input)),
-      })
-      return schema
-    },
-
-    'max': (max: number) => {
-      cs.push({
-        '~i': () => import('./max'),
-        '~id': () => import('./max').then(m => m.maxSymbol),
-        '~c': (input: number) => import('./max').then(m => m.buildMaxCheck(max)(input)),
-      })
-      return schema
-    }
-
-    '~b': () => 7,
-  }
-
-  return schema
+  return 1 as any
 }
