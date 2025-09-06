785d274a517fbc2ba3c1bf5c7340d0112e671757
schplitt <schplitt.contact@gmail.com>
2025-09-06 22:07:37 +0200

    test

---
+++ packages/spur/src/types/utils.ts
@@ -0,0 +1,6 @@
+import type { BuildableSchema } from './schema'
+
+export type Prettify<T> = { [K in keyof T]: T[K] } & {}
+
+export type ExtractOutputType<T extends BuildableSchema<any, any, any>> = T extends BuildableSchema<infer TOutput, any, any> ? TOutput : never
+export type ExtractInputType<T extends BuildableSchema<any, any, any>> = T extends BuildableSchema<any, infer TInput, any> ? TInput : never
