0b58dd09f7c0fb40b9d684e11f3856edc22d5f0c
schplitt <schplitt.contact@gmail.com>
2025-09-28 00:19:00 +0200

    refactor: simplify ExtractSchemas and ExtractOptions types for better readability

--- packages/spur/src/options/options.ts
+++ packages/spur/src/options/options.ts
@@ -25,27 +25,27 @@ export type InferOptionalityType<T extends CommonOptions> = T extends { optional
  * Extracts all schemas from T that have the option TKey set to TValue
  * E.g. to extract all optional schemas from a union of schemas
  */
-export type ExtractSchemas<T extends BuildableSchema<any, any, CommonOptions>, TKey extends keyof CommonOptions, TValue extends CommonOptions[TKey]> = T extends BuildableSchema<any, any, infer TCommonOptions> ? TCommonOptions extends { [K in TKey]: TValue } ? T : never : never
+export type ExtractSchemas<T extends BuildableSchema, TKey extends string, TValue> = T extends BuildableSchema<any, any, infer TCommonOptions> ? TCommonOptions extends { [K in TKey]: TValue } ? T : never : never
 
 /**
  * Extracts the options from a schema
  */
-export type ExtractOptions<T extends BuildableSchema<any, any, CommonOptions>> = T extends BuildableSchema<any, any, infer TCommonOptions> ? TCommonOptions : never
+export type ExtractOptions<T extends BuildableSchema> = T extends BuildableSchema<any, any, infer TCommonOptions> ? TCommonOptions : never
 
 export type MakeOptional<TOptions extends CommonOptions> = SetCommonOption<TOptions, 'optionality', 'optional'>
-export type ExtractOptionalSchema<T extends BuildableSchema<any, any, CommonOptions>> = ExtractSchemas<T, 'optionality', 'optional'>
+export type ExtractOptionalSchema<T extends BuildableSchema> = ExtractSchemas<T, 'optionality', 'optional'>
 
 export type MakeRequired<TOptions extends CommonOptions> = SetCommonOption<TOptions, 'optionality', 'required'>
-export type ExtractRequiredSchema<T extends BuildableSchema<any, any, CommonOptions>> = ExtractSchemas<T, 'optionality', 'required'>
+export type ExtractRequiredSchema<T extends BuildableSchema> = ExtractSchemas<T, 'optionality', 'required'>
 
 export type MakeNullable<TOptions extends CommonOptions> = SetCommonOption<TOptions, 'optionality', 'nullable'>
-export type ExtractNullableSchema<T extends BuildableSchema<any, any, CommonOptions>> = ExtractSchemas<T, 'optionality', 'nullable'>
+export type ExtractNullableSchema<T extends BuildableSchema> = ExtractSchemas<T, 'optionality', 'nullable'>
 
 export type MakeNullish<TOptions extends CommonOptions> = SetCommonOption<TOptions, 'optionality', 'nullish'>
-export type ExtractNullishSchema<T extends BuildableSchema<any, any, CommonOptions>> = ExtractSchemas<T, 'optionality', 'nullish'>
+export type ExtractNullishSchema<T extends BuildableSchema> = ExtractSchemas<T, 'optionality', 'nullish'>
 
 export type MakeDefaulted<TOptions extends CommonOptions> = SetCommonOption<TOptions, 'optionality', 'defaulted'>
-export type ExtractDefaultedSchema<T extends BuildableSchema<any, any, CommonOptions>> = ExtractSchemas<T, 'optionality', 'defaulted'>
+export type ExtractDefaultedSchema<T extends BuildableSchema> = ExtractSchemas<T, 'optionality', 'defaulted'>
 
 export type MakeUndefinable<TOptions extends CommonOptions> = SetCommonOption<TOptions, 'optionality', 'undefinable'>
-export type ExtractUndefinableSchema<T extends BuildableSchema<any, any, CommonOptions>> = ExtractSchemas<T, 'optionality', 'undefinable'>
+export type ExtractUndefinableSchema<T extends BuildableSchema> = ExtractSchemas<T, 'optionality', 'undefinable'>
