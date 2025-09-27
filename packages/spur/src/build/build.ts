0bc0659a660a6d884eb190342730193a6bd4107d
schplitt <schplitt.contact@gmail.com>
2025-09-27 23:25:20 +0200

    chore: prepare for multiple schema `builds`

---
+++ packages/spur/src/build/build.ts
@@ -0,0 +1,7 @@
+import type { EvaluableSchema } from '../types/schema'
+
+export interface BuildEvaluableSchemaInput<TOutput> {
+
+}
+
+export function buildEvaluableSchema<TOutput>(input: BuildEvaluableSchemaInput<TOutput>): EvaluableSchema<TOutput> {}
