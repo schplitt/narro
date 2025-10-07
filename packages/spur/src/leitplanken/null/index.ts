d390dc5858d8e9ec3baee690bd256a1cdd3c68d1
schplitt <schplitt.contact@gmail.com>
2025-10-07 17:03:35 +0200

    remove for now

--- packages/spur/src/leitplanken/null/index.ts
+++ packages/spur/src/leitplanken/null/index.ts
@@ -1,14 +1,13 @@
 import type { BuildableSchema } from '../../types/schema'
-import { build } from '../../build'
 
 export interface NullSchema extends BuildableSchema<null, null> {
 
 }
 
-export function _null(): NullSchema {
+/* export function _null(): NullSchema {
   return {
     '@build': () => {
-      return build(() => import('./null').then(m => m.default()))
     },
   }
 }
+ */
--- packages/spur/src/leitplanken/undefined/index.ts
+++ packages/spur/src/leitplanken/undefined/index.ts
@@ -1,16 +1,14 @@
 import type { BuildableSchema } from '../../types/schema'
-import { build } from '../../build'
 
 export interface UndefinedSchema extends BuildableSchema<undefined, undefined> {
 
 }
 
-function _undefined(): UndefinedSchema {
+/* function _undefined(): UndefinedSchema {
   return {
     '@build': () => {
-      return build(() => import('./undefined').then(m => m.default()))
     },
   }
 }
 
-export { _undefined as undefined }
+export { _undefined as undefined } */
